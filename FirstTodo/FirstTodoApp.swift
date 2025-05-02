25982f159bd50c60a87a491e69234f82f2b8ca9c
data-bs <databslee@gmail.com>
2025-05-02 23:49:24 +0900

    Initial Commit

---
+++ FirstTodo/FirstTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstTodoApp.swift
+//  FirstTodo
+//
+//  Created by Beomseok Lee on 5/2/25.
+//
+
+import SwiftUI
+
+@main
+struct FirstTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FirstTodoTests/FirstTodoTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirstTodoTests.swift
+//  FirstTodoTests
+//
+//  Created by Beomseok Lee on 5/2/25.
+//
+
+import Testing
+@testable import FirstTodo
+
+struct FirstTodoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
